7a3cd968d06a9dddbab904029754ada55f97a79c
Stevie-Ray Hartog <stevierayhartog@bluebirdday.nl>
2020-10-23 00:00:20 +0200

    update 1.7.0

--- app/src/main/java/nl/stevie/ray/hangtime/LauncherActivity.java
+++ app/src/main/java/nl/stevie/ray/hangtime/LauncherActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.stevie_ray.hangtime.twa;
+package nl.stevie.ray.hangtime;
 
 import android.net.Uri;
 
@@ -21,6 +21,9 @@ import android.net.Uri;
 
 public class LauncherActivity
         extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+    
+
+    
 
     @Override
     protected Uri getLaunchingUrl() {
