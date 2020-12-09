32fc3038e6c59e520f6ae04c22c65b43a8158e34
Stevie-Ray Hartog <stevierayhartog@bluebirdday.nl>
2020-12-10 00:00:00 +0100

    1.8

---
+++ app/src/main/java/nl/stevie/ray/hangtime/DelegationService.java
@@ -0,0 +1,14 @@
+package nl.stevie.ray.hangtime;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
