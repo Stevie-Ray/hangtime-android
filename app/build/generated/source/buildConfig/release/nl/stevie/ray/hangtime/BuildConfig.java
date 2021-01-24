586f3f28836ee327bffb320b2048922815e488f3
Stevie-Ray Hartog <stevierayhartog@bluebirdday.nl>
2021-01-24 02:23:10 +0100

    play billing support

---
+++ app/build/generated/source/buildConfig/release/nl/stevie/ray/hangtime/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nl.stevie.ray.hangtime;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "nl.stevie.ray.hangtime";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 19000917;
+  public static final String VERSION_NAME = "0.9.17";
+}
